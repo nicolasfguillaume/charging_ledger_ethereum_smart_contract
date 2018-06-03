adccbc30b9eb4846b20976ec6f452548afe9ff30
Nicolas Guillaume <nicolas.f.guillaume@gmail.com>
2018-06-04 00:00:25 +0200

    Add files via upload

---
+++ charging_ledger_contract.sol
@@ -0,0 +1,73 @@
+pragma solidity ^0.4.0;
+
+contract charge_database {
+
+   struct Charge {
+       address user_id;
+       uint32 charger_id;
+       uint32 start;
+       uint32 duration;
+       uint8 mode;
+       uint256 bonus;
+   }
+   
+   //mapping (address => User) Users;
+   address[] usersByAddress;
+   
+   mapping (bytes32 => Charge) Charges; 
+   bytes32[] chargesList; 
+
+   uint32[] contractsList;
+   
+   function addNewCustomer(address user_id) public returns (bool success) {
+      usersByAddress.push(user_id);
+      return true;
+  }
+  
+  function addNewCharge(  address user_id, 
+                          uint32 charger_id,
+                          uint32 start,
+                          uint32 duration,
+                          uint8 mode,
+                          uint256 bonus
+                          ) public returns (bytes32) {
+
+    bytes32 chargeId = keccak256(user_id, charger_id, start);
+    Charges[chargeId] = Charge(user_id, charger_id, start, duration, mode, bonus);
+    
+    //address thisNewAddress = msg.sender;
+
+      //Charges[thisNewAddress].user_id = user_id;
+      //Charges[thisNewAddress].charger_id = charger_id;
+      //Charges[thisNewAddress].start = start;
+      //Charges[thisNewAddress].duration = duration;
+      //Charges[thisNewAddress].mode = mode;
+      //Users[thisNewAddress].balance += bonus;
+      chargesList.push(chargeId);
+      return chargeId;
+  }
+
+  function getUsersList() public constant returns (address[]) { 
+      return usersByAddress; 
+  }
+
+  function getChargesList() public constant returns (bytes32[]) { 
+      return chargesList; 
+  }
+
+  function getChargeData(bytes32 chargeId) public constant returns (
+                                   uint32,
+                                   uint32,
+                                   uint32,
+                                   uint8,
+                                  uint256
+                                   ) {
+    return (Charges[chargeId].charger_id,
+            Charges[chargeId].start,
+            Charges[chargeId].duration,
+            Charges[chargeId].mode,
+            Charges[chargeId].bonus
+            );
+  }
+
+}
